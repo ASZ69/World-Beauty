4621a90f8291ccc92cceaca2ac86189f7bcd1923
shafqat692 <shafqat69169@gmail.com>
2019-01-23 22:26:10 +0500

    index modified and map

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 // JavaScript Document
 
 $(document).ready(function(){
-	
+//	$('#vid').get(0).play();
 });
+
+
+//contact
+	function sendMail() {
+			var name = document.getElementById("name").value;
+			var email = document.getElementById("email").value;
+			var num = document.getElementById("num").value;
+			var sub = document.getElementById("sub").value;
+			var msg = document.getElementById("msg").value;
+			window.location.href = "mailto:" + email + "?subject=" + sub + "&body=I'm " + name + " " + msg;
+		}
