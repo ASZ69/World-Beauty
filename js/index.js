947fda145f799bc4f9b4fd15784381adf119d221
shafqat692 <shafqat69169@gmail.com>
2019-01-25 21:20:53 +0500

    anim

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
 // JavaScript Document
 
 $(document).ready(function(){
-//	$('#vid').get(0).play();
+	$(function() {
+
+	  $('.long-div').appear();
+
+	  $(document.body).on('appear', '.long-div', function(e, $affected) {
+		// this code is executed for each appeared element
+		$affected.each(function() {
+		  $('.long-div').addClass('slideInRight');
+		});
+		  
+	  });
+	});
+
 });
-
-
 //contact
-	function sendMail() {
-			var name = document.getElementById("name").value;
-			var email = document.getElementById("email").value;
-			var num = document.getElementById("num").value;
-			var sub = document.getElementById("sub").value;
-			var msg = document.getElementById("msg").value;
-			window.location.href = "mailto:" + email + "?subject=" + sub + "&body=I'm " + name + " " + msg;
-		}
+//	function sendMail() {
+//			var name = document.getElementById("name").value;
+//			var email = document.getElementById("email").value;
+//			var num = document.getElementById("num").value;
+//			var sub = document.getElementById("sub").value;
+//			var msg = document.getElementById("msg").value;
+//			window.location.href = "mailto:" + email + "?subject=" + sub + "&body=I'm " + name + " " + msg;
+//		}
