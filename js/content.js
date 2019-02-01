078bb7a7e9e2713b0f68cbb91d1231eb191398c5
shafqat692 <shafqat69169@gmail.com>
2019-02-01 19:31:43 +0500

    content

--- js/content.js
+++ js/content.js
@@ -8,31 +8,3 @@ content.controller("navControl",function($scope,$filter){
 		$scope.contentArray = $filter('filter')(navList,{name:'7Wonders'});
 	}
 });
-var navList = [{name:'7Wonders'},
-			  pageContent:[
-			   {imgScr:'../images/africa/payramids-round.jpg',
-			   moreDetails:{
-			       wikipedia:'https://en.wikipedia.org/wiki/Great_Pyramid_of_Giza',
-			       images:[
-			          '../images/africa/payramids-round.jpg',
-			          '../images/africa/payramids-round.jpg']
-			   
-			  info:'some information about pyramids as fuck as possible' }},
-			  {imgScr:'../images/africa/payramids-round.jpg',
-			   moreDetails:{
-			       wikipedia:'https://en.wikipedia.org/wiki/Great_Pyramid_of_Giza',
-			       images:[
-			          '../images/africa/payramids-round.jpg',
-			          '../images/africa/payramids-round.jpg']
-			   
-			  info:'some information about pyramids as fuck as possible' }},
-                    {imgScr:'../images/africa/payramids-round.jpg',
-			   moreDetails:{
-			       wikipedia:'https://en.wikipedia.org/wiki/Great_Pyramid_of_Giza',
-			       images:[
-			          '../images/africa/payramids-round.jpg',
-			          '../images/africa/payramids-round.jpg']
-			   
-			  info:'some information about pyramids as fuck as possible' }}
-			 ]
-		  ]
