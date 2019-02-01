f64102b2728b99fd1c2e8c1f7e5736350031eaa3
awais69 <awais69169@gmail.com>
2019-02-01 21:24:15 +0500

    ja mze kr

--- js/content.js
+++ js/content.js
@@ -1,10 +1,140 @@
 // JavaScript Document
-var content=angular.module("content",[]);
+var content = angular.module("myApp",[]);
 
 content.controller("navControl",function($scope,$filter){
 	$scope.showContent=function(content){
 		var referClick=content.currentTarget;
 		var navigation= referClick.innerHTML;
-		$scope.contentArray = $filter('filter')(navList,{name:'7Wonders'});
+		$scope.contentArray = $filter('filter')(myData,{name:navigation});
 	}
+	
+	$scope.contentArray = $filter('filter')(myData, {name: 'Pakistan'});
+	
 });
+
+
+
+var myData = [
+	{name:"Pakistan",
+	 
+	 places:[
+		 
+		 {pName:"K2",
+		  imgSrc:"../images/africa/vic_falls_feat_Safrica.jpg",
+		  heading:"My K2",
+		  rating:"1.1",
+		  smallDescrip:"DescriptionMauritius, an Indian Ocean island nation, is known for its beaches, lagoons and reefs.",
+		  wiki:"https://www.en.wikipedia.com/",
+		  
+		  moreDetails:{
+			  
+			  images:[
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg"
+			  ],
+			  
+			  largeDescription:"jkshfkash"
+		  }
+		  
+		 },
+		 
+		 {pName:"L2",
+		  imgSrc:"../images/africa/cape-town.jpg",
+		  heading:"My L2",
+		  rating:"9.0",
+		  smallDescrip:"charh kh test kr lo",
+		  wiki:"https://www.en.wikipedia.com/l2",
+		  
+		  moreDetails:{
+			  
+			  images:[
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg"
+			  ],
+			  
+			  largeDescription:"jkshfkash"
+		  }
+		  
+		 },
+		 
+		 {pName:"G2",
+		  imgSrc:"../images/africa/cape-town.jpg",
+		  heading:"My L2",
+		  rating:"9.0",
+		  smallDescrip:"charh kh test kr lo",
+		  wiki:"https://www.en.wikipedia.com/l2",
+		  
+		  moreDetails:{
+			  
+			  images:[
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg"
+			  ],
+			  
+			  largeDescription:"jkshfkash"
+		  }
+		  
+		 }
+		 
+	 ]
+	
+	},
+	
+	{name:"India",
+	 
+	 places:[
+		 
+		 {pName:"K2",
+		  imgSrc:"../images/africa/cape-town.jpg",
+		  heading:"My K2",
+		  rating:"1.1",
+		  smallDescrip:"alaw jaga hai",
+		  wiki:"https://www.en.wikipedia.com/",
+		  
+		  moreDetails:{
+			  
+			  images:[
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg"
+			  ],
+			  
+			  largeDescription:"jkshfkash"
+		  }
+		  
+		 },
+		 
+		 {pName:"L2",
+		  imgSrc:"../images/africa/cape-town.jpg",
+		  heading:"My L2",
+		  rating:"9.0",
+		  smallDescrip:"charh kh test kr lo",
+		  wiki:"https://www.en.wikipedia.com/l2",
+		  
+		  moreDetails:{
+			  
+			  images:[
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg",
+				  "images/j.jpg"
+			  ],
+			  
+			  largeDescription:"jkshfkash"
+		  }
+		  
+		 }
+		 
+	 ]
+	
+	}
+
+];
